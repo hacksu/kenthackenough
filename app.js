998ca3fa2d9f36b4b3bfb5e118fe5d9fddf5c4a2
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-11 23:40:33 -0500

    Upgrade to node 12.0

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('\n  🎉  Kent Hack Enough listening at http://%s:%s\n', host, port);
+  console.log('\n  🎉  Kent Hack Enough listening at %s%s\n', host, port);
 });
 
 // Connect to database

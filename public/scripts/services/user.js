aef2ef55c5d029639a8a3bc3f99fccd7a052a3c5
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-18 17:58:05 -0500

    Implement user API interface

--- public/scripts/services/user.js
+++ public/scripts/services/user.js
@@ -1,41 +1,89 @@
 /**
 * User model
-* Register a user, store the logged-in user, and logout
+* Connects to the /users part of the API
+* Also stores user locally via cookies
 */
-
 angular
-  .module('kheStaff')
-  .service('User', ['$http', '$scope', function ($http, $scope) {
+  .module('khe')
+  .factory('User', ['$http', '$cookieStore', function ($http, $cookies) {
 
-    return {
+    var User = function () {
 
-      register: function () {
+      /**
+      * Store the user locally
+      * @param me An object representing the logged-in user
+      *           {email: String, password: String, role: String}
+      */
+      this.setMe = function (me) {
+        $cookies.put('me', me);
+      };
+
+      /**
+      * Retrieve the logged in user from local storage
+      * @return {email: String, password: String, role: String}
+      */
+      this.getMe = function () {
+        return $cookies.get('me');
+      };
+
+      /**
+      * Delete the stored user
+      */
+      this.removeMe = function () {
+        $cookies.remove('me');
+      };
+
+      /**
+      * Register a user
+      * @param email The email to register with
+      * @param password The password to register with
+      * @return An $http promise
+      */
+      this.register = function (email, password) {
         var req = {
           method: 'POST',
           url: '/users/register',
           data: {
-            email: $scope.me.email,
-            password: $scope.me.password
+            email: email,
+            password: password
           }
         };
-        $http(req)
-          .success(function (data) {
-            $scope.me.loggedIn = true;
-          })
-          .error(function (data, status) {
-            $scope.me.loggedIn = false;
-            console.log(data);
-          });
-      },
+        return $http(req);
+      };
 
-      me: function () {
-        return $scope.me;
-      },
+      /**
+      * Activate a user
+      * @param userId The user's ID
+      * @return An $http promise
+      */
+      this.activate = function (userId) {
+        var req = {
+          method: 'GET',
+          url: '/users/activate/' + userId
+        };
+        return $http(req);
+      };
 
-      logout: function () {
-        $scope.me = {};
-      }
+      /**
+      * Login a user
+      * @param email The email to attempt with
+      * @param password The password to attempt with
+      * @return An $http promise
+      */
+      this.login = function (email, password) {
+        var req = {
+          method: 'POST',
+          url: '/users/login',
+          data: {
+            email: email,
+            password: password
+          }
+        };
+        return $http(req);
+      };
 
     };
+
+    return User;
 
   }]);

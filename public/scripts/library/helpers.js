b3bbe606b4615cf4fa81ea815e4bebe507a7a7de
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-20 04:25:31 -0500

    Add phone number formatter

--- public/scripts/library/helpers.js
+++ public/scripts/library/helpers.js
@@ -50,6 +50,13 @@ function base64Encode(data) {
   return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
 }
 
+function formatPhoneNumber(s) {
+  var s2 = (""+s).replace(/\D/g, '');
+  var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+  return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+}
+
 var Helpers = {
-  base64Encode: base64Encode
+  base64Encode: base64Encode,
+  formatPhoneNumber: formatPhoneNumber
 };

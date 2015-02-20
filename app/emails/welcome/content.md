a9c07ef480bea182fabe852857e01e102a38ff9e
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-19 22:31:04 -0500

    Mailer works, add welcome message

---
+++ app/emails/welcome/content.md
@@ -0,0 +1,5 @@
+## This was created with markdown
+1. One
+1. Two
+1. Three
+1. Four
--- app/helpers/mailer.js
+++ app/helpers/mailer.js
@@ -1,19 +1,108 @@
+/**
+* A helper to send mass emails with nodemailer and jade
+* @author Paul Dilyard
+*
+* Usage:
+* Prerequisites:
+*   - process.env.GMAIL_USERNAME    // => user.name@gmail.com
+*   - process.env.GMAIL_PASSWORD    // => password123
+*   - process.env.FROM_EMAIL        // => First Last <first.last@example.com>
+*
+* To create a template:
+*   - Create a new folder inside of app/emails with the name of your template
+*   - Create two files inside this folder: html.jade and text.txt
+*   - Build the HTML version of the template inside html.jade
+*   - Build the plaintext version of the template inside text.txt
+*
+* To send a message:
+*   var Message = require('./mailer');
+*   var message = new Message({
+*     template: 'welcome',
+*     subject: 'Welcome',
+*     recipients: [
+*       {
+*         email: 'name@example.com',
+*         locals: {
+*           localKey: 'localValue'
+*         }
+*       }
+*     ]
+*   });
+*   message.send();
+*/
+
 var path = require('path');
 var templatesDir = path.resolve(__dirname, '..', 'emails');
-var emailTemplates = require('email-templates');
+var fs = require('fs');
+var jade = require('jade');
 var nodemailer = require('nodemailer');
 var winston = require('winston');
+var flow = require('flow');
 
-module.exports = function () {
+module.exports = function (options) {
 
-  emailTemplates(templatesDir, function (err, template) {
-    if (err) {
-      winston.error(err);
-    } else {
+    var self = this;
+    self.options = options || {};
 
+    return {
 
+      send: function () {
 
-    }
-  })
+        // Create our transporter object
+        var transporter = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: process.env.GMAIL_USERNAME,
+            pass: process.env.GMAIL_PASSWORD
+          }
+        });
+
+        // Read in html.jade and text.txt
+        var pathToHtml = path.resolve(templatesDir, self.options.template, 'html.jade');
+        var pathToTxt = path.resolve(templatesDir, self.options.template, 'text.txt');
+
+        flow.exec(
+          function () {
+            // Read in the jade file
+            fs.readFile(pathToHtml, 'utf8', this);
+          },
+          function (err, data) {
+            // Read in the txt file
+            if (err) throw err;
+            this.html = data;
+            fs.readFile(pathToTxt, 'utf8', this);
+          },
+          function (err, data) {
+            if (err) throw err;
+            this.txt = data;
+            this();
+          },
+          function () {
+            var data = this;
+            // Compile the jade file
+            var fn = jade.compile(data.html, {filename: pathToHtml});
+            // Iterate over our recipients
+            for (var i = 0; i < self.options.recipients.length; ++i) {
+              var recipient = self.options.recipients[i];
+              // Configure the message
+              var mailOptions = {
+                from: process.env.FROM_EMAIL,
+                to: recipient.email,
+                subject: self.options.subject,
+                text: data.txt,
+                html: fn(recipient.locals)
+              };
+              // Send the email
+              transporter.sendMail(mailOptions, function (err, info) {
+                if (err) throw err;
+                winston.info('Email sent: ' + info.response);
+              });
+            }
+          }
+        ); // end of flow
+
+      }
+
+    };
 
 };

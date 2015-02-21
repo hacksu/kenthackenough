3732a6cb82aa204821ec4c994eb79ff96c1ae2f6
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-21 01:00:40 -0500

    Fix filters

--- public/scripts/controllers/staff/attendees.js
+++ public/scripts/controllers/staff/attendees.js
@@ -45,7 +45,7 @@ angular
     // Display users that have RSVPd yes
     self.going = function () {
       self.current = self.users.filter(function (user) {
-        return user.application.going;
+        return user.application.going == 'Yes';
       });
     };
 

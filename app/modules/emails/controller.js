b2f779836c5e76be412043915b65ae2c6d6af46b
Paul Dilyard <paul.dilyard@gmail.com>
2015-02-22 18:43:40 -0500

    Write routes for emails

---
+++ app/modules/emails/controller.js
@@ -0,0 +1,32 @@
+var router = getRouter();
+var Email = require('./model');
+var User = require('../users/model');
+
+/**
+* Send an email
+* POST: (see docs)
+* AUTH: admin, staff
+*/
+router.post('/emails/send', User.Auth([User.ADMIN, User.STAFF]), function (req, res) {
+  console.log(req.body);
+  var errors = Email.validate(req.body);
+  if (errors.length) return res.multiError(errors);
+  var email = new Email(req.body);
+  email.save(function (err, email) {
+    if (err) return res.internalError();
+    var message = new Email.Message(email);
+    message.send();
+    return res.send({});
+  });
+});
+
+/**
+* Get a list of emails
+* AUTH: admin, staff
+*/
+router.get('/emails', User.Auth([User.ADMIN, User.STAFF]), function (req, res) {
+  Email.find({}, function (err, emails) {
+    if (err) return res.internalError();
+    return res.send({emails: emails});
+  });
+});

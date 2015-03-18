069ffdad1e7dd6a84da303717d6a68f181773c53
Paul Dilyard <paul.dilyard@gmail.com>
2015-03-18 03:15:59 -0500

    Write tests for URL shortener

--- test/test.js
+++ test/test.js
@@ -461,6 +461,102 @@ describe('API', function () {
 
   }); // end User
 
+  describe('URL Shortener', function () {
+
+    var urlId;
+
+    /**
+    * Create a new shortened URL
+    */
+    it('should create a new shortened URL', function (done) {
+      request(app)
+        .post('/urls')
+        .auth(adminKey, adminToken)
+        .send({
+          full: 'http://example.com',
+          short: 'shortenedexample'
+        })
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          res.body.should.have.property('_id');
+          res.body.should.have.property('full');
+          res.body.should.have.property('short');
+          res.body.full.should.equal('http://example.com');
+          res.body.short.should.equal('shortenedexample');
+          urlId = res.body._id;
+          done();
+        });
+    });
+
+    /**
+    * Resolve a shortened URL
+    */
+    it('should resolve a shortened URL', function (done) {
+      request(app)
+        .get('/urls/go/shortenedexample')
+        .expect(301)
+        .end(function (err, res) {
+          if (err) throw err;
+          done();
+        });
+    });
+
+    /**
+    * Get a single URL
+    */
+    it('should get a single URL', function (done) {
+      request(app)
+        .get('/urls/'+urlId)
+        .auth(adminKey, adminToken)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          res.body.should.have.property('_id');
+          res.body.should.have.property('full');
+          res.body.should.have.property('short');
+          res.body.full.should.equal('http://example.com');
+          res.body.short.should.equal('shortenedexample');
+          done();
+        });
+    });
+
+    /**
+    * Get a list of URLs
+    */
+    it('should get a list of URLs', function (done) {
+      request(app)
+        .get('/urls')
+        .auth(adminKey, adminToken)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          res.body.should.have.property('urls');
+          res.body.urls.length.should.be.above(0);
+          res.body.urls[0].should.have.property('_id');
+          res.body.urls[0].should.have.property('full');
+          res.body.urls[0].should.have.property('short');
+          done();
+        });
+    });
+
+    /**
+    * Delete a URL
+    */
+    it('should delete a URL', function (done) {
+      request(app)
+        .delete('/urls/'+urlId)
+        .auth(adminKey, adminToken)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          res.body.should.have.property('_id');
+          done();
+        })
+    });
+
+  }); // end URL Shortener
+
   // Remove the admin user
   after(function (done) {
     User.remove({email: 'admin@test.com'}, function (err) {

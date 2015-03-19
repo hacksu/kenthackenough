1d7dbff1507cf12499fb2acd567a7fb5f7e7a434
Paul Dilyard <paul.dilyard@gmail.com>
2015-03-19 12:52:34 -0400

    Rewrite emails API

--- app/emails/controller.js
+++ app/emails/controller.js
@@ -4,29 +4,50 @@ var User = require('../users/model');
 
 /**
 * Send an email
-* POST: (see docs)
-* AUTH: admin, staff
+* POST /emails
+* Auth -> admin
 */
-router.post('/emails/send', User.auth('admin'), function (req, res) {
+router.post('/emails', User.auth('admin'), function (req, res) {
   var errors = Email.validate(req.body);
   if (errors.length) return res.multiError(errors);
   req.body.sent = Date.now();
   var email = new Email(req.body);
   email.save(function (err, email) {
     if (err) return res.internalError();
-    var message = new Email.Message(email);
-    message.send();
-    return res.send({});
+    if (process.env.NODE_ENV == 'production') {
+      var message = new Email.Message(email);
+      message.send();
+    }
+    return res.json(email);
   });
 });
 
 /**
-* Get a list of emails
-* AUTH: admin, staff
+* Get a list of sent emails
+* GET /emails
+* Auth -> admin, staff
 */
 router.get('/emails', User.auth('admin', 'staff'), function (req, res) {
-  Email.find({}, function (err, emails) {
-    if (err) return res.internalError();
-    return res.send({emails: emails});
-  });
+  Email
+    .find()
+    .exec(function (err, emails) {
+      if (err) return res.internalError();
+      return res.json({emails: emails});
+    });
+});
+
+/**
+* Delete a sent email
+* DELETE /emails/:id
+* Auth -> admin
+*/
+router.delete('/emails/:id', User.auth('admin'), function (req, res) {
+  Email
+    .findByIdAndRemove(req.params.id)
+    .exec(function (err, email) {
+      if (err) res.internalError();
+      return res.json({
+        _id: email._id
+      });
+    });
 });

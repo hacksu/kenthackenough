c2c86ae61946f9cebc91692e94eec5d417f58106
Paul Dilyard <paul.dilyard@gmail.com>
2015-03-31 10:20:27 -0400

    Add sudo to provision

--- provision.sh
+++ provision.sh
@@ -1,36 +1,36 @@
 # nginx and node
-apt-get update
-apt-get install -y nginx nodejs npm
-npm install -g npm
-npm install -g n mocha
+sudo apt-get update
+sudo apt-get install -y nginx nodejs npm
+sudo npm install -g npm
+sudo npm install -g n mocha
 n stable
 
 # mongodb
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-apt-get update
-apt-get install -y mongodb-org
-service mongod start
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
 
 # redis
-apt-get install -y redis-server
-service redis-server start
+sudo apt-get install -y redis-server
+sudo service redis-server start
 
 # nginx config
 if [ -f /etc/nginx/sites-available/default ]; then
-  rm /etc/nginx/sites-available/default
-  rm /etc/nginx/sites-enabled/default
+  sudo rm /etc/nginx/sites-available/default
+  sudo rm /etc/nginx/sites-enabled/default
 fi
-cp /var/www/kenthackenough/config/api.khe.conf /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/api.khe.conf /etc/nginx/sites-enabled
-service nginx reload
+sudo cp /var/www/kenthackenough/config/api.khe.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/api.khe.conf /etc/nginx/sites-enabled
+sudo service nginx reload
 
 # install libraries
 cd /var/www/kenthackenough
 npm install
 
 # pm2
-npm install -g pm2
+sudo npm install -g pm2
 
 echo "-----------------------"
 echo "| test:     npm test  |"

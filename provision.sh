9ee90ffb0b64c51f8e0cb0d5de4a744fa124a0ad
Paul Dilyard <paul.dilyard@gmail.com>
2015-08-04 22:07:37 -0500

    Match mongo version with production

--- provision.sh
+++ provision.sh
@@ -10,7 +10,7 @@ n 0.12.0
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
-apt-get install -y mongodb-org=2.6.9 mongodb-org-server=2.6.9 mongodb-org-shell=2.6.9 mongodb-org-mongos=2.6.9 mongodb-org-tools=2.6.9
+apt-get install -y mongodb-org=2.6.10 mongodb-org-server=2.6.10 mongodb-org-shell=2.6.10 mongodb-org-mongos=2.6.10 mongodb-org-tools=2.6.10
 service mongod start
 
 # redis

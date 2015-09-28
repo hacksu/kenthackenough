c1ebc3a5b84d008775f16433a2448268e43fcf54
Paul Dilyard <paul.dilyard@gmail.com>
2015-09-28 11:16:42 -0400

    Only log to stdout when not in production or test

--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -2,16 +2,32 @@
 
 let bunyan = require('bunyan');
 
-module.exports = bunyan.createLogger({
-  name: 'khe',
-  streams: [
-    {
-      level: 'info',
-      stream: process.stdout
-    },
-    {
-      level: 'error',
-      path: '/var/tmp/kenthackenough.log'
-    }
-  ]
-});
+if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'test') {
+
+  module.exports = bunyan.createLogger({
+    name: 'khe',
+    streams: [
+      {
+        level: 'warn',
+        path: '/var/tmp/kenthackenough.log'
+      }
+    ]
+  });
+
+} else {
+
+  module.exports = bunyan.createLogger({
+    name: 'khe',
+    streams: [
+      {
+        level: 'info',
+        stream: process.stdout
+      },
+      {
+        level: 'warn',
+        path: '/var/tmp/kenthackenough.log'
+      }
+    ]
+  });
+
+}

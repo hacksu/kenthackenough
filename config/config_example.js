b1131bc6cf18c9757a999ce092f9373398a0ed9e
Isaac Park <isaac.a.park+git@gmail.com>
2018-02-08 15:19:14 -0500

    Add comma after logo

--- config/config_example.js
+++ config/config_example.js
@@ -54,7 +54,7 @@ module.exports = {
   },
   
   // The location that sponsor logos are saved to.
-  logoDir: 'logos/'
+  logoDir: 'logos/',
 
   // The location that resumes are saved to.
   resumeDir: 'resumes/'

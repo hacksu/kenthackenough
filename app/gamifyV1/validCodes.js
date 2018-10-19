b2999242420bdf0a22d356d90a9a226747d67207
Brojowski <alex.gajowski@gmail.com>
2018-10-19 16:43:20 -0400

    valid codes for event

--- app/gamifyV1/validCodes.js
+++ app/gamifyV1/validCodes.js
@@ -1,10 +1,148 @@
 'use strict';
 
 module.exports = {
-    example: {
-        pointID: 123,
-        points: 0,
-        sponsorerID: 'things',
-        reason: 'stuff'
-      }
+        ibm83: {
+          pointID: 1,
+          points: 50,
+          sponsorerID: "ibm",
+          reason: 1
+        },
+        prog0: {
+          pointID: 2,
+          points: 50,
+          sponsorerID: "progressive",
+          reason: 2
+        },
+        mri23: {
+          pointID: 3,
+          points: 50,
+          sponsorerID: "mri",
+          reason: 3
+        },
+        hy430: {
+          pointID: 4,
+          points: 50,
+          sponsorerID: "hyland",
+          reason: 4
+        },
+        rovi7: {
+          pointID: 5,
+          points: 50,
+          sponsorerID: "rovisys",
+          reason: 5
+        },
+        mesh2: {
+          pointID: 6,
+          points: 50,
+          sponsorerID: "remesh",
+          reason: 6
+        },
+        over0: {
+          pointID: 7,
+          points: 50,
+          sponsorerID: "overdrive",
+          reason: 7
+        },
+        assu1: {
+          pointID: 8,
+          points: 50,
+          sponsorerID: "assurant",
+          reason: 8
+        },
+        good4: {
+          pointID: 9,
+          points: 50,
+          sponsorerID: "goodyear",
+          reason: 9
+        },
+        drun6: {
+          pointID: 10,
+          points: 50,
+          sponsorerID: "drund",
+          reason: 10
+        },
+        air70: {
+          pointID: 11,
+          points: 50,
+          sponsorerID: "air everywhere",
+          reason: 11
+        },
+        cs852: {
+          pointID: 12,
+          points: 50,
+          sponsorerID: "ksu_cs",
+          reason: 12
+        },
+        ds402: {
+          pointID: 13,
+          points: 50,
+          sponsorerID: "ksu_ds",
+          reason: 13
+        },
+        laun7: {
+          pointID: 14,
+          points: 50,
+          sponsorerID: "launchnet",
+          reason: 14
+        },
+        tech5: {
+          pointID: 15,
+          points: 50,
+          sponsorerID: "techstyle",
+          reason: 15
+        },
+        open4: {
+          pointID: 16,
+          points: 50,
+          sponsorerID: "opening_ceremony",
+          reason: 16
+        },
+        hub89: {
+          pointID: 17,
+          points: 50,
+          sponsorerID: "github_lesson",
+          reason: 17
+        },
+        webst: {
+          pointID: 18,
+          points: 50,
+          sponsorerID: "web_dev_lesson",
+          reason: 18
+        },
+        e5cap: {
+          pointID: 19,
+          points: 50,
+          sponsorerID: "escape_room",
+          reason: 19
+        },
+        angnn: {
+          pointID: 20,
+          points: 50,
+          sponsorerID: "angular_web_lesson",
+          reason: 20
+        },
+        crypt: {
+          pointID: 21,
+          points: 50,
+          sponsorerID: "crypto_$_lesson",
+          reason: 21
+        },
+        dux40: {
+          pointID: 22,
+          points: 50,
+          sponsorerID: "redux_lesson",
+          reason: 22
+        },
+        sta54: {
+          pointID: 23,
+          points: 50,
+          sponsorerID: "cup_stacking",
+          reason: 23
+        },
+        pong4: {
+          pointID: 24,
+          points: 50,
+          sponsorerID: "water_pong",
+          reason: 24
+        }
 };

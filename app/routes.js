8d13bcdb4d0cc249297a46f81498a2cfeb610856
cseitz5 <cseitz5@kent.edu>
2020-09-03 13:18:33 -0400

    a

--- app/routes.js
+++ app/routes.js
@@ -36,7 +36,7 @@ module.exports = function (router) {
   */
 
   // debug version to check if deploy actually works
-  router.get('/debug_version', (req, res) => res.send('1.3'));
+  router.get('/debug_version', (req, res) => res.send('1.4'));
 
   // about
   router.put('/about', auth('admin', 'staff'), about.put);

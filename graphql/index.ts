87a4372dd692f95b84e3e9209e6c100a04125e12
cseitz5 <cseitz5@kent.edu>
2021-08-31 15:36:02 -0400

    imported graphql-scalars for enhanced graphql validation

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,17 +1,61 @@
-import { gql } from 'apollo-server-micro'
-import { Config } from 'apollo-server-micro'
+import { gql, Config } from 'apollo-server-micro'
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { typeDefs as ScalarTypeDefs, resolvers as ScalarResolvers } from 'graphql-scalars';
+
+// export const ApolloServerConfig: Config = {
+//     typeDefs: gql`
+//         type Query {
+//             hello: String
+//         }
+//     `,
+//     resolvers: {
+//         Query: {
+//             hello() {
+//                 return 'hi there!'
+//             }
+//         }
+//     }
+// }
+
+let phone = '+12261234567';
 
 export const ApolloServerConfig: Config = {
-    typeDefs: gql`
-        type Query {
-            hello: String
-        }
-    `,
-    resolvers: {
-        Query: {
-            hello() {
-                return 'hi there!'
+    schema: makeExecutableSchema({
+        typeDefs: [
+            ...ScalarTypeDefs,
+            gql`
+                scalar PhoneNumber
+
+                type Query {
+                    hello: String
+                    phone: PhoneNumber
+                }
+
+                type Mutation {
+                    updatePhone(to: PhoneNumber): PhoneNumber
+                }
+            `,
+
+        ],
+        resolvers: {
+            ...ScalarResolvers,
+            Query: {
+                hello() {
+                    return "hi there!"
+                },
+                phone() {
+                    return phone
+                }
+            },
+            Mutation: {
+                updatePhone(_, { to }) {
+                    phone = to;
+                    return phone;
+                }
             }
+        },
+        parseOptions: {
+            commentDescriptions: true,
         }
-    }
+    })
 }

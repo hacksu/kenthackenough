45a5c519951d1ef8e3f58f67d02a8c9266865a4c
cseitz5 <cseitz5@kent.edu>
2021-09-03 16:44:38 -0400

    init

---
+++ graphql/index.js
@@ -0,0 +1,17 @@
+import { gql } from 'apollo-server-express'
+
+
+export const ApolloServerConfig = {
+    typeDefs: gql`
+        type Query {
+            hello: String
+        }
+    `,
+    resolvers: {
+        Query: {
+            hello() {
+                return 'hi there!'
+            }
+        }
+    }
+}
